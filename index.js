3a7ae28f472db1fa80a88de8c0e0e3914078f44a
IMGSS <838632526@qq.com>
2017-04-19 09:46:15 -0500

    改为异步函数统计阅读量

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var onepage = require('./onepage');
 var promiseArr = [],
     host = 'http://www.cnblogs.com/';
 host += process.argv[2] ? process.argv[2] : 'imgss' + '/';
+//改为异步函数写法。
+/**promise 
 for(let i = 1; i < 10; i++) {
     promiseArr.push(onepage(host, i))
 }
@@ -12,3 +14,16 @@ Promise.all(promiseArr).then(function(data) {
     }
     console.log(sum);
 })
+*/
+let i=1,sum=0;
+(async function getAll(){
+while(true){
+    let num= await onepage(host , i++);
+    if(num){
+    sum += num;  
+    }else{
+        break;
+    }
+}
+console.log('总阅读量:',sum);
+})();
